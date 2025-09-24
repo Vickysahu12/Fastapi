2d27c44a721d105f39c9c6b53de3815f038baf84
Vickysahu12 <bikisahu161@gmail.com>
2025-09-24 11:24:04 +0530

    learn how to setup and work with async sqlalchemy

---
+++ chapter24/app/main.py
@@ -0,0 +1,15 @@
+from tables.tables import create_tables
+from services.services import *
+import asyncio
+
+async def main():
+    # Create tables
+    #await create_tables()
+    
+    # Create User
+    #await create_user("Sweety", "sweety@example.com")
+    #await create_user("vicky","vicky@example.com")
+
+    print(await get_all_user())
+
+asyncio.run(main())
---
+++ chapter24/app/tables/tables.py
@@ -0,0 +1,16 @@
+from sqlalchemy import MetaData, Column, Table, Integer, String
+from database.db import engine
+
+metadata = MetaData()
+
+users = Table(
+    "users",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("name", String(length=50), nullable=False),
+    Column("email",String, nullable=False, unique=True)
+)
+
+async def create_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(metadata.create_all)
