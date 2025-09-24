95f46d230c15af68ee518a433eed43b51ace66fe
Vickysahu12 <bikisahu161@gmail.com>
2025-09-24 22:46:09 +0530

    Lets make it

--- chapter27/app/db/config.py
+++ chapter27/app/db/config.py
@@ -6,3 +6,5 @@ DATABASE_URL = "sqlite:///bunny.db"
 engine = create_engine(DATABASE_URL,echo=True)
 
 SessionLocal = sessionmaker(bind=engine,expire_on_commit=False)
+
+# Lets from tomoorrow making the most famous news app of this generation FlashFeed
