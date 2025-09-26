5fcaf9608d3c8c1f84fae1710961c144ffdcd584
Vickysahu12 <bikisahu161@gmail.com>
2025-09-26 19:21:59 +0530

    learn some imp part

--- chapter27/app/db/config.py
+++ chapter27/app/db/config.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-DATABASE_URL = "sqlite:///bunny.db"
+db_path = os.path.join(BASE_DIR,"bunny.db")
+
+DATABASE_URL = f"sqlite:///{db_path}"
 
 engine = create_engine(DATABASE_URL,echo=True)
 
