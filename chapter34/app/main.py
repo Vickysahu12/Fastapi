b2f2680f292dc4df03bceccc7aaefb6dde52b329
Vickysahu12 <bikisahu161@gmail.com>
2025-11-13 21:14:01 +0530

    Learning about Creating and Using dependency injection

---
+++ chapter34/app/main.py
@@ -0,0 +1,26 @@
+# Learning about Creating and Using dependency injection
+
+from fastapi import FastAPI,Depends
+from typing import Annotated
+
+app = FastAPI()
+
+# Creating dependency function:
+async def common_parameter(q:str | None = None, skip:float = 0, limit:int = 100):
+    return {"q": q, "skip": skip, "limit": limit}
+
+# Using dependency in endpoint:
+@app.get("/items")
+async def read_items(commons:Annotated[dict,Depends(common_parameter)]):
+    return commons
+
+@app.get("/users")
+async def read_users(commons:Annotated[dict,Depends(common_parameter)]):
+    return commons
+
+# we can create Type Alias to not writing the code again and again
+commonsDep = Annotated[dict, Depends(common_parameter)] 
+
+@app.get("/Product")
+async def read_product(commons:commonsDep):
+    return commons
