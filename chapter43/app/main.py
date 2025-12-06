ce465499dd1c66fc97a323389b2db16a24270b61
Vickysahu12 <bikisahu161@gmail.com>
2025-12-06 20:27:02 +0530

    refreshment

--- chapter43/app/main.py
+++ chapter43/app/main.py
@@ -9,3 +9,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.get("/items")
 async def all_items():
     return "all items"
+
+@app.get("/me")
+async def me():
+    return "hey this is me 'vicky'"
