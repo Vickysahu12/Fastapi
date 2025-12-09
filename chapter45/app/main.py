e06b68a3000ba41f53dd38c73e13b340703a2760
Vickysahu12 <bikisahu161@gmail.com>
2025-12-09 22:26:51 +0530

    learn about .env

---
+++ chapter45/app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env files
+load_dotenv()
+
+app = FastAPI()
+
+@app.get("/")
+def read_env():
+    return{
+        "api-secret": os.getenv("API_SECRET_KEY"),
+        "debug_mode": os.getenv("DEBUG")
+    }
