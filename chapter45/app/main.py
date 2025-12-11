1d1c13fc221a5d87ea7b632afbf142c08b44f871
Vickysahu12 <bikisahu161@gmail.com>
2025-12-11 14:34:27 +0530

    learn abiut decouple

--- chapter45/app/main.py
+++ chapter45/app/main.py
@@ -1,23 +1,28 @@
 from fastapi import FastAPI
-from dotenv import load_dotenv
-import os
+from decouple import config
+#from dotenv import load_dotenv
+# import os
 
-# Load environment variables from .env files
-load_dotenv()
+# # Load environment variables from .env files
+# load_dotenv()
+
+# app = FastAPI()
+
+# @app.get("/")
+# def read_env():
+#     return{
+#         "api-secret": os.getenv("API_SECRET_KEY"),
+#         "debug_mode": os.getenv("DEBUG")
+#     }
+
+# Lets learn about python decouple
 
 app = FastAPI()
 
 @app.get("/")
 def read_env():
     return{
-        "api-secret": os.getenv("API_SECRET_KEY"),
-        "debug_mode": os.getenv("DEBUG")
-    }
-
-@app.get("/me")
-def read_env():
-    return{
-        "api-secret": os.getenv("API_SECRET_KEY"),
-        "debug_mode": os.getenv("DEBUG")
+        "api-secret":config("API_SECRET_KEY"),
+        "debug_mode":config("DEBUG")
     }
 
